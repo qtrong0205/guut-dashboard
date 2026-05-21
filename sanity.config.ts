59215a0a8d5d02585e52603ee938e3642237d682
qtrong0205 <quoctrong02052006@gmail.com>
2026-05-21 18:29:00 +0700

    refactor: remove siteSettings schema and clean up sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
 import project from './schemas/project'
 import post from './schemas/post'
 import teamMember from './schemas/teamMember'
-import siteSettings from './schemas/siteSettings'
 
 export default defineConfig({
   name: 'default',
   title: 'Guu & T',
-  projectId: '08k96exq',  
+  projectId: '08k96exq',
   dataset: 'production',
   plugins: [structureTool()],
-  schema: { types: [project, post, teamMember, siteSettings] },
+  schema: {types: [project, post, teamMember]},
 })
